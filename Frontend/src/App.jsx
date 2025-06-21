e27dde2b88479d25cc43af4c6eb5dc45b9bd98d9
vishalidinesh1998 <vishalidinesh1998@gmail.com>
2025-06-21 16:38:24 +0530

    update app file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,74 +7,74 @@ import * as XLSX from 'xlsx'
 
 function App() {
 
-  const [msg,setmsg]=useState("")
-  const [status,setstatus]=useState(false)
-  const [emaillist,setemaillist]=useState([])
+  const [msg, setmsg] = useState("")
+  const [status, setstatus] = useState(false)
+  const [emaillist, setemaillist] = useState([])
 
-  function handlemessage(evt){
+  function handlemessage(evt) {
     setmsg(evt.target.value)
 
   }
 
-  function handlefile(evt){
-  const file = evt.target.files[0]; // ✅ fixed from event.target
+  function handlefile(evt) {
+    const file = evt.target.files[0]; // ✅ fixed from event.target
 
-  const reader = new FileReader();
+    const reader = new FileReader();
 
-  reader.onload = function(event) {
-    const data = event.target.result;
-    const workbook = XLSX.read(data, { type: 'binary' });
-    const sheetName = workbook.SheetNames[0];
-    const worksheet = workbook.Sheets[sheetName]; // ✅ fixed from SheetName
-    const emaillist = XLSX.utils.sheet_to_json(worksheet, { header: 'A' });
-    const totalemail=emaillist.map(function(item){
-      return(item.A)
-    })
-    setemaillist(totalemail)
-  };
+    reader.onload = function (event) {
+      const data = event.target.result;
+      const workbook = XLSX.read(data, { type: 'binary' });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName]; // ✅ fixed from SheetName
+      const emaillist = XLSX.utils.sheet_to_json(worksheet, { header: 'A' });
+      const totalemail = emaillist.map(function (item) {
+        return (item.A)
+      })
+      setemaillist(totalemail)
+    };
 
-  reader.readAsBinaryString(file);
-}
+    reader.readAsBinaryString(file);
+  }
 
 
-  function send(){
-      setstatus(true)
-      axios.post("http://localhost:5000/sendmail",{msg:msg,emaillist:emaillist})
-      .then(function(data){
-        if(data.data===true){
+  function send() {
+    setstatus(true)
+    axios.post("https://bulkmail-1-zbw5.onrender.com/sendmail", { msg: msg, emaillist: emaillist })
+      .then(function (data) {
+        if (data.data === true) {
           alert("Email Send Successfully")
           setstatus(false)
         }
-        else{
+        else {
           alert("Failed to Send")
         }
       })
   }
 
-  return(
+  return (
     <div>
-    <div className="p-2 bg-blue-950 text-2xl text-white font-bold text-center">
-      <h1>Bulk Mail</h1>
-    </div>
-    <div className="p-2 bg-blue-900 text-xl text-white font-semibold text-center">
-      <h1>We can help your business with sending multiple emails at once</h1>
-    </div>
-    <div className="p-2 bg-blue-800 text-xl text-white font-semibold text-center">
-      <h1>Drag and Drop</h1>
-    </div>
-       <div className="p-4 bg-blue-600   text-center">
-      <textarea onChange={handlemessage} value={msg} className='border-black rounded w-80 h-32' placeholder='Enter the email text.....'></textarea>
-    </div>
-        <div className="p-2 bg-blue-500   text-center ">
-      <input onChange={handlefile} type="file" className='border-4 border-dashed  p-4 mt-5 mb-5'></input>
-    </div>
+      <div className="p-2 bg-blue-950 text-2xl text-white font-bold text-center">
+        <h1>Bulk Mail</h1>
+      </div>
+      <div className="p-2 bg-blue-900 text-xl text-white font-semibold text-center">
+        <h1>We can help your business with sending multiple emails at once</h1>
+      </div>
+      <div className="p-2 bg-blue-800 text-xl text-white font-semibold text-center">
+        <h1>Drag and Drop</h1>
+      </div>
+      <div className="p-4 bg-blue-600   text-center">
+        <textarea onChange={handlemessage} value={msg} className='border-black rounded w-80 h-32' placeholder='Enter the email text.....'></textarea>
+      </div>
+      <div className="p-2 bg-blue-500   text-center ">
+        <input onChange={handlefile} type="file" className='border-4 border-dashed  p-4 mt-5 mb-5'></input>
+      </div>
       <div className="p-2 bg-blue-300    text-center ">
-    <p>Total Email in the file:{emaillist.length}</p>
-    </div>
+        <p>Total Email in the file:{emaillist.length}</p>
+      </div>
 
-       <div className="p-2 bg-blue-200    text-center ">
-      <button onClick={send} className='bg-blue-950 text-white p-2 rounded'>{status?"Sending...":"Send"}</button>
-    </div>
+      <div className="p-2 bg-blue-200    text-center ">
+        <button onClick={send} className='bg-blue-950 text-white p-2 rounded'>{status ? "Sending..." : "Send"}</button>
+      </div>
     </div>
   )
 }
